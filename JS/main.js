6dfb5d4b675a8821cdfe106a33d6c505cb2c8ec2
Gendis Atiqa Hadhi <gendisatiqahadhi17072009@gmail.com>
2025-03-18 15:56:37 +0700

    [FIX] portofolio-nya

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+/* // console.log("halo dunia");
+// document.write("saya senang sekali")
+*/
+
+let nama = "Gendis Atiqa Hadhi";
+console.log(nama);
+//console.log(typeof(nama));
+nama = 1234;
+//console.log(nama);
+console.log(typeof(nama));
+
+let kotak = null;
+
+
+
+// var nama1
+// const nama2
